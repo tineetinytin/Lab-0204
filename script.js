1d2761eaf7ff25797c1995a0320a19dbc017ae47
tineetinytin <christine.elene@gmail.com>
2021-02-04 21:03:59 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+    Project: Lab 01: My First JavaScript Application
+    Date: 2/4/21
+    Author: Zak Ruvacalba
+
+
+*/
+
+
+let num = prompt('Guess a number between 1 and 10:');
+if (num == 5) {
+    alert('You guessed the right number!');
+} else {
+    alert('You guessed the wrong number!');
+}
